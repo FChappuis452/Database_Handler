b92a7343955cc464446907bff573c04a62e34153
FChappuis452 <fchappuis3050@conestogac.on.ca>
2018-01-14 19:50:28 -0500

    Created Testing app for Db Class

---
+++ DbTester/DbTester/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+* Project			:   Db_Testing
+*
+* File				:   Program.cs
+*
+* Programmer		: 	Frederic Chappuis
+*
+* First Version		: 	January 14, 2018
+*
+* Description		:	This is a sinple console program to test the functions of the Logger and Db class
+*/
+using Database_Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = "lllocalhost";
+            string port = "3306";
+            string db = "lab4";
+            string user = "testAccount";
+            string password = "password";
+
+            //Generates a successfull connections
+            DbHandler Db = new DbHandler(ip, port, db, user, password);
+            Db.ConnectToDb();
+           
+          
+        }
+    }
+}
