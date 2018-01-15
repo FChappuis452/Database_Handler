2ee1f829f2425d8bb9b491e27b93e67af69d7fee
FChappuis452 <fchappuis3050@conestogac.on.ca>
2018-01-14 19:49:57 -0500

    Created connection code and Constructors

--- Database_Handler/Database_Handler/DbHandler.cs
+++ Database_Handler/Database_Handler/DbHandler.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+* NAME          :   DbHandler   
+* 
+* FIRST VERSION :   January 14, 2018
+*  
+* PURPOSE       :   The purpose of this class is to connect to a MySQL database and be able to 
+*                   perform SQL commands.
+*               
+*                   This uses MySql.Data reference which was aqcuired at https://dev.mysql.com/downloads/mysql/
+*/
+
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +20,52 @@ namespace Database_Handler
 {
     public class DbHandler
     {
-        //test
+        private MySqlConnection Connect { set; get; }
+        private string ConnectionString { set; get; }
+
+
+
+        // Constructor with no IpAddress or port selected, will default to localhost:3306
+        public DbHandler(string user, string password, string database)
+            : this("127.0.0.1", "3306", database, user, password) { }
+
+        // Constructor with no selected database
+        public DbHandler(string IpAddress, string user, string port, string password)        
+            : this(IpAddress, port, "", user, password) { }
+
+        // Default Constructor
+        public DbHandler(string IpAddress, string port, string database, string user,  string password)
+        {
+            ConnectionString = $"Server={IpAddress};Port={port};Database={database};Uid={user};Pwd={password};";
+        }
+
+
+
+
+        /*
+        * Function		:	ConnectToDb
+        *
+        * Description	:   This function will take the ConnectionString and attempt to connect to the 
+        *                   database
+        *	
+        * Parameters	:   none
+        *	
+        * Return Value	:	void	
+        */
+        public void ConnectToDb()
+        {
+            string logStart = Logger.LogMessageStart("DbHandler", "ConnectToDb");
+
+            try
+            {
+                Connect = new MySqlConnection(ConnectionString);
+                Logger.WriteLog(logStart + "Connection Successful");
+                Connect.Open();
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog(logStart + "Connection ERROR: " + ex.Message);
+            }
+        }
     }
 }
---
+++ Database_Handler/Database_Handler/Logger.cs
@@ -0,0 +1,81 @@
+﻿/*
+* NAME          :   Logger   
+*                   
+* FIRST VERSION :   January 14, 2018
+* 
+* PURPOSE       :   The purpose of the logger class will be to log the events in the DB class. 
+*                   It will create a log file in the root folder where the application is running.
+*                   
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_Handler
+{
+    public static class Logger
+    {
+        
+        public static string Filename { set; get; } 
+
+        /*
+        * Function		:	WriteLog
+        *
+        * Description	:   This will write the log to a file.  The file will be name
+        *                   according to date
+        *	
+        * Parameters	:   string message: the message being written to file
+        *	
+        * Return Value	:	datatype	
+        */
+        public static void WriteLog(string message)
+        {
+            DateTime date = DateTime.Today;   
+            Filename = date.ToString("MMM-dd-yyyy") + ".log";
+
+            try
+            {
+                lock (Filename)
+                {
+                    using (StreamWriter writer = new StreamWriter(Filename, true))
+                    {
+                        writer.WriteLine(message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter writer = new StreamWriter(Filename, true))
+                {
+                    writer.WriteLine("(Logging ERROR: " + ex.Message);
+                }
+            }
+        }
+
+
+
+
+        /*
+        * Function		:   LogMessageStart
+        *
+        * Description	:   This will create the header for the log file message
+        *                   The header will be current time (24hr HH:mm:ss) the className.method
+        *                   ex. 20:30:45 [classname.method] 
+        *	
+        * Parameters	:   string className    :   The originating class where the logging event happens
+        *                   string method       :   The originating method where the logging event happens
+        *	
+        * Return Value	:	string  : Will return the formatted string
+        */
+        public static string LogMessageStart(string className, string method)
+        {
+            DateTime date = DateTime.Now;
+            string message = date.ToString("HH:mm:ss") + " [" + className + "." + method + "] ";
+            return message;
+        }
+    }
+}
